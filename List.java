983dceda4217a54e366d89a151950724a92c12ea
Piyush Acharya <acharyapiyush1@outlook.com>
2022-09-10 20:42:29 -0700

    Checks if the user wants to enter another number or not

--- List.java
+++ List.java
@@ -4,32 +4,51 @@ public class List {
     private static Node[] list;
     static int start;
 
-    public static void add(Integer num, int start) {
+    public static void add(Integer num, int start, int whereToStore) {
         Node n = new Node();
         n.data = num;
-        for (int i = start; i < list.length; i++) {
-            if (list[i] == null) {
-                list[i] = n;
-                break;
-            }
-        }
+        list[whereToStore] = n;
     }
 
-    public static void main(String[] args) {
-        list = new Node[10];
+    public static void main(String[] args) throws InterruptedException {
+        list = new Node[100];
         start = 2;
 
+        // Get user input
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 10; i++) {
-            System.out.println("\nEnter the next number: ");
-            int input = Integer.parseInt(sc.nextLine());
-            add(input, start);
+        boolean running = true;
+        while (running) {
+            for (int i = 0; i < 100; i++) {
+                sc = new Scanner(System.in);
+                System.out.println("Do you want to input another number? y/n");
+                String yesno = sc.nextLine();
+                if (yesno.equals("n")) {
+                    running = false;
+                    break;
+                }
+                System.out.println("\nEnter the next number: ");
+                int input = Integer.parseInt(sc.nextLine());
+
+                if (i != 0) {
+                    System.out.println("Index: ");
+                    int whereToStore = Integer.parseInt(sc.nextLine());
+                    add(input, start, whereToStore);
+                } else {
+                    add(input, start, start);
+                }
+            }
         }
         sc.close();
 
+        for (int i = 0; i < 100; i++) {
+            System.out.println();
+            Thread.sleep(5);
+        }
+
+        // Print all values in the list
         for (int j = start; j < list.length; j++) {
             if (list[j] != null) {
-                System.out.println(j + ":" + list[j].data);
+                System.out.println(j + ": " + list[j].data);
             }
         }
     }
