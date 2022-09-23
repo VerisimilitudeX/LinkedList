61b3c61211bde7ab1d6d1ac94550a8c6336459d5
Piyush Acharya <acharyapiyush1@outlook.com>
2022-09-22 21:34:26 -0700

    Added constructor

--- Node.java
+++ Node.java
@@ -2,4 +2,9 @@ public class Node {
     public Integer data;
     public int nextLocation;
     public int previousLocation;
+    public Node() {
+        data = null;
+        nextLocation = -1;
+        previousLocation = -1;
+    }
 }
